e4d98cf2f1ee92103cec4f626e66453c6fbeaccf
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-12 14:54:22 +1000

    Added class Project

---
+++ src/Project.js
@@ -0,0 +1,14 @@
+export default class Project {
+    constructor(name) {
+        this.name = name
+        this.tasks = []
+    }
+
+    addTask(task) {
+        this.tasks.push(task)
+    }
+
+    removeTask(index) {
+        this.tasks.splice(index, 1)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css'
+
+import Project from './Project'
+
+const a = new Project('a')
+a.addTask("test")
+console.log(a)
+a.addTask("b")
+console.log(a)
+a.removeTask(0)
+console.log(a)
