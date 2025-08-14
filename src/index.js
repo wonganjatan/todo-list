493ca23892a574ec9b7c43ca544e16224e3da6f3
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-15 08:38:19 +1000

    CHanged from javascript to html for the skeleton:

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import './styles.css'
 import Project from './Project'
 import Task from './Task'
 
-const content = document.querySelector('.content')
-
-const sideBar = document.createElement('div')
-const sideBarProfile = document.createElement('div')
-const sideBarTaskNav = document.createElement('div')
-const sideBarProjectnav = document.createElement('div')
-const sideBarTeamNav = document.createElement('div')
-
-sideBar.classList.add('.sidebar')
-sideBarProfile.classList.add('.sidebar-profile')
-sideBarTaskNav.classList.add('.sidebar-tasknav')
-sideBarProjectnav.classList.add('.sidebar-projectnav')
-sideBarTeamNav.classList.add('.sidebar-teamnv')
-
 // const myProject = new Project()
 // myProject.addTask(new Task("title", "test", 1, "high"))
 // console.log(myProject);
