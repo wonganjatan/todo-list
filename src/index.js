717e6e155b3ecf2b4afc7092ec4553cc84a0e7a6
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-15 08:33:10 +1000

    Added sidebar and its child divs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import './styles.css'
 import Project from './Project'
 import Task from './Task'
 
-const myProject = new Project()
-myProject.addTask(new Task("title", "test", 1, "high"))
-console.log(myProject);
+const content = document.querySelector('.content')
+
+const sideBar = document.createElement('div')
+const sideBarProfile = document.createElement('div')
+const sideBarTaskNav = document.createElement('div')
+const sideBarProjectnav = document.createElement('div')
+const sideBarTeamNav = document.createElement('div')
+
+sideBar.classList.add('.sidebar')
+sideBarProfile.classList.add('.sidebar-profile')
+sideBarTaskNav.classList.add('.sidebar-tasknav')
+sideBarProjectnav.classList.add('.sidebar-projectnav')
+sideBarTeamNav.classList.add('.sidebar-teamnv')
+
+// const myProject = new Project()
+// myProject.addTask(new Task("title", "test", 1, "high"))
+// console.log(myProject);
 
 
 // const a = new Project('a')
